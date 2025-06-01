191140e5c8aac2d7eba140f24573d3e63342d5d9
Alexander Koskovich <akoskovich@pm.me>
2025-06-01 09:54:34 -0400

    asteroids: Drop face unlock HAL
    Even with AOSP IFace implemented, face unlock is not functional
    without extra platform work.
    
    Change-Id: I67b4c5e82fd7af8517cd0736037495a5445f65a9

--- device.mk
+++ device.mk
@@ -252,7 +252,6 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Biometrics
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.biometrics.face.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Bluetooth
